a6fd9d10b6fe2f0b097fdc9b156e169c74ff611a
Enzodacabbie <lvcabrera@cpp.edu>
2023-03-16 19:03:29 -0700

    added unit test for log controller

--- app/controllers/LogController.php
+++ app/controllers/LogController.php
@@ -1,5 +1,5 @@
 <?php
-// Test controller
+
 use Monolog\Level;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
@@ -7,15 +7,6 @@ use Monolog\Handler\StreamHandler;
 require_once(__DIR__ . "/../bootstrap.php");
 
 class LogController extends BaseController {
-   public function mod($number) {
-      if (FALSE === is_numeric($number["number"]))
-      {
-         $this->setResponse("Please enter a number");
-      }
-      else{
-         $this->setResponse($number["number"]." modulo by 10 is ".$number["number"] % 10);
-      }
-   }
 
    public function log() {
       $log = new Logger('name');
