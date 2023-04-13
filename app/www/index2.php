bfe26d2664d65d8c1adc05167894a75ff5d072d6
Enzodacabbie <lvcabrera@cpp.edu>
2023-04-13 12:16:19 -0700

    test

--- app/www/index2.php
+++ app/www/index2.php
@@ -79,7 +79,7 @@ if($controller !== "" && $method !== "") {
 <body>
   <h1>Welcome to Classroom Navigator</h1>
   <div>
-    <input id="name_search" type="text"></input>
+    <input id="test name_search" type="text"></input>
     <button class="btn btn-primary" onclick="search()">Search</button>  
   </div>
   <div>
