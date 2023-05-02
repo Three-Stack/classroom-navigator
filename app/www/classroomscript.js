3caa06ef64ca3c551242f972d38d70452da200c1
KnightinGale9 <99992991+KnightinGale9@users.noreply.github.com>
2023-05-01 20:34:28 -0700

    adding the last page variables

--- app/www/classroomscript.js
+++ app/www/classroomscript.js
@@ -6,15 +6,12 @@ function im(image) {
   document.getElementById(image[0]).src = eval(image + ".src")
 }
 
-
 const output = document.querySelector('.output');
-const leftside = document.createElement('div');
-leftside.className="leftside";
-output.append(leftside);
 const url = '/Search/getRoomInfo' + window.location.search;
-var i=1;
+
 window.addEventListener('DOMContentLoaded',()=>{
-   loadData();
+  var i=1; 
+  loadData();
 })
 function loadData(){
    fetch(url).then(rep=>rep.json())
@@ -23,26 +20,25 @@ function loadData(){
       addtoPage(data);
    })
 }
-function addtoPage(arr){
-  arr.forEach((el)=>{
+function addtoPage(data){
+    console.log(data.room_info)
     const row=document.createElement('div');
     row.className="row row-col-4 no-gutters";
     const col1 = document.createElement('div');
     col1.className="col no-gutters";
-    col1.textContent=el.classname;
+    col1.textContent=data.classname;
     row.append(col1);
     const col2 = document.createElement('div');
     col2.className="col no-gutters";
-    col2.textContent=el.room_info;
+    col2.textContent=data.room_info;
     row.append(col2);
     const col3 = document.createElement('div');
     col3.className="col no-gutters";
-    col3.textContent=el.instructor;
+    col3.textContent=data.instructor;
     row.append(col3);
     const col4 = document.createElement('div');
     col4.className="col no-gutters";
-    col4.textContent=el.time;
+    col4.textContent=data.time;
     row.append(col4);
-    leftside.append(row);
-  });
-}
+    output.append(row);
+  };
